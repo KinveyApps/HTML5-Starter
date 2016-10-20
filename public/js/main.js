c625025009d64f348953af0213d298a6064885a0
Thomas Conner <thomas@kinvey.com>
2016-10-20 13:45:10 -0400

    Remove appKey and appSecret

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 // Initialize Kinvey
 // Replace appKey and appSecret with your apps credentials
 Kinvey.init({
-  appKey: 'kid_WJt3WXdOpx',
-  appSecret: '7cfd74e7af364c8f90b116c835f92e7d'
+  appKey: '',
+  appSecret: ''
 });
 
 
